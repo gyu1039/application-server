f35e373b7f3a7b6fd828bcb15244b584860f9486
Ingyu <gyu1039@naver.com>
2022-04-29 14:54:13 +0900

    index.html 응답하기 - 리팩토링 전

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +29,26 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
+        	
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "utf8"));
+            String inputLine;
+            List<String> tokens;
+            
+        	while((inputLine = br.readLine()) != null) {
+        		log.debug(inputLine);
+        		tokens = List.of(inputLine.split(" "));
+        		if(tokens.contains("/index.html")) {
+        			String url = "/index.html";
+        			byte[] body2 = Files.readAllBytes(new File("./webapp" + url).toPath());
+        			DataOutputStream dos = new DataOutputStream(out);
+        			response200Header(dos, body2.length);
+        			responseBody(dos, body2);
+        		}
+        		
+        		if(inputLine.equals("")) break;
+        	}
+        	
+        	DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
